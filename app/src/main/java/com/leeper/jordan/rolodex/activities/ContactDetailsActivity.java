69ec65d7cae945a985643e076d558878d3c1e4be
jmleep <j.m.leep@gmail.com>
2015-12-03 23:29:38 -0600

    Changes to include fragment in this activity

--- app/src/main/java/com/leeper/jordan/rolodex/activities/ContactDetailsActivity.java
+++ app/src/main/java/com/leeper/jordan/rolodex/activities/ContactDetailsActivity.java
@@ -1,34 +1,37 @@
 package com.leeper.jordan.rolodex.activities;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
 
 import com.leeper.jordan.rolodex.R;
 import com.leeper.jordan.rolodex.datasource.Contact;
+import com.leeper.jordan.rolodex.fragments.ContactDetailsFragment;
 
 /**
  * Created by jordan on 11/23/15.
  */
 public class ContactDetailsActivity extends AppCompatActivity {
 
-    private TextView name;
-    private TextView phone;
-    private TextView email;
+    private final FragmentManager fragmentManager = getSupportFragmentManager();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_details);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         Contact contactToView = (Contact) getIntent().getExtras().get("contactToView");
+        Bundle bundle = new Bundle();
+        bundle.putParcelable("contactToView", contactToView);
 
-        name = (TextView) findViewById(R.id.contact_details_name);
-        phone = (TextView) findViewById(R.id.contact_details_phone);
-        email = (TextView) findViewById(R.id.contact_details_email);
+        ContactDetailsFragment contactDetailsFragment = new ContactDetailsFragment();
+        contactDetailsFragment.setArguments(bundle);
 
-        name.setText(contactToView.getName());
-        phone.setText(contactToView.getPhone());
-        email.setText(contactToView.getEmail());
+        fragmentManager.beginTransaction().add(R.id.details_content_frame, contactDetailsFragment).commit();
+
     }
 }
